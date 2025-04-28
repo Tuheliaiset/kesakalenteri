834c5c5f6a8b5b12ba78f734245f2b668427bae8
bhu270@myy.haaga-helia.fi <bhu270@myy.haaga-helia.fi>
2025-04-28 15:38:38 +0300

    js sivulle vähän muistiinpanoja

---
+++ toiminnot.js
@@ -0,0 +1,6 @@
+// nappia painettaessa tulostuu ikkuna jossa näkyy tekemisvinkkejä kesälle
+// ikkuna ilmestyy vain jos ehdot täyttyvät 
+// jokaiselle napille on määritetty millä aikavälillä se on mahdollista avata
+// ehtona click toiminnon toteutumiselle on että tämän hetkinen päivämäärä vastaa napille määritettyä aikaväliä
+// jos ehto ei täyty, tulostuu lause tyylillä "Malta odottaa vielä hetki ..."
+// ehtoon kuuluu myös että jos napin määritetty aika on jo mennyt, se on avattavissa
