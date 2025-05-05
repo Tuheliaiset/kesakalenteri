9d25de286784a0267767c6e1cb6a04d71e06437f
kajsa.lindroos@myy.haaga.helia.fi <kajsa.lindrooos@gmail.com>
2025-05-05 13:20:18 +0300

    päivämäärät js

--- toiminnot.js
+++ toiminnot.js
@@ -14,7 +14,7 @@ console.log(paivatanaan);
 
 
 function KesaKalenteri1() {
-    if (paivatanaan >= new Date("2025-04-19")) {
+    if (paivatanaan >= new Date("2024-05-19")) {
         document.getElementById("otsikko").innerHTML = "Linnanmäen hupietupäivä 19.05.2025";
         document.getElementById("kuva").src = "kuvat/pexels-silvia-17374-89505 (1).jpg";
         document.getElementById("kappale").innerHTML = "Hanki ranneke edullisempaan hintaan:<br>Isohupi 49,00EUR (normaalihinta 53,00EUR)<br>Pikkuhupi 41,00EUR (normaalihinta 43,00EUR)<br><br>Hupietupäivinä S-ryhmästä saa rannekkeen vielä tätäkin edulliemmin. S-etukortilla Isohupi 47 e pikkuhupi 39 e. S-etuhintaiset liput ovat myynnissä Prismoissa, Sokoksissa ja S‑marketeissa sekä osassa pääkaupunkiseudun Sokos- ja Radisson Blu -hotelleista majoituksen yhteydessä. Rannekkeiden osto ei kerrytä S‑ryhmän Bonusta.​";
@@ -26,7 +26,7 @@ function KesaKalenteri1() {
 
 }
 function KesaKalenteri2() {
-    if (paivatanaan >= new Date("2025-04-26")) {
+    if (paivatanaan >= new Date("2024-05-26")) {
         document.getElementById("otsikko").innerHTML = "Nuorten aikuisten yökahvila 30.05.2025";
         document.getElementById("kuva").src = "kuvat/VK2.jpg";
         document.getElementById("kappale").innerHTML = "Nuorten päihteettömään yökahvilaan ovat tervetulleita kaikki 18–29-vuotiaat nuoret. Yökahvilassa voi hengailla, opiskella, jutella ja tutustua muihin nuoriin. Paikalla on Sirkus Magentan kaksi sirkusohjaajaa, joiden kanssa voi halutessaan kokeilla sirkusvälineitä ja -lajeja turvallisesti. Sirkustilassa on mahdollisuus esimerkiksi ilma-akrobatian, trampoliinin, jongleerauksen, yksipyöräisen ja lattia-akrobatian kokeiluun. Yökahvila järjestetään sirkustilassamme Itäkeskuksessa Helsingissä kerran viikossa perjantaisin klo 20:30-23:30 ";
@@ -37,7 +37,7 @@ function KesaKalenteri2() {
     }
 }
 function KesaKalenteri3() {
-    if (paivatanaan >= new Date("2025-04-02")) {
+    if (paivatanaan >= new Date("2025-06-02")) {
         document.getElementById("otsikko").innerHTML = "Opastettu kierros Suomenlinnassa kesä-elokuussa päivittäin klo 12";
         document.getElementById("kuva").src = "kuvat/VK3.jpg";
         document.getElementById("kappale").innerHTML = "Opastetulla kävely­kierroksella kuljetaan historiallisilla paikoilla, ja kuullaan linnoituksen kiehtovasta menneisyydestä ja nykypäivästä. Kierrokset alkavat Suomenlinna-museolta.<br><br>Liput on hyvä ostaa ennakkoon.<br><br>Liput 15e (aikuiset), 5e (lapset 7-17v)";
@@ -49,7 +49,7 @@ function KesaKalenteri3() {
 
 }
 function KesaKalenteri4() {
-    if (paivatanaan >= new Date("2025-04-09")) {
+    if (paivatanaan >= new Date("2025-06-09")) {
         document.getElementById("otsikko").innerHTML = "Helsinki Samba Carnaval kulkue 14.06.2025 klo 15 alkaen ";
         document.getElementById("kuva").src = "kuvat/VK4.jpg";
         document.getElementById("kappale").innerHTML = "Tule seuraamaan Suomen sambakoulujen kulkuetta.<br><br>Reitti: Senaatintori – Unioninkatu – Eteläesplanadi – Mannerheimintie – Aleksanterinkatu<br><br>Helsinki Samba Carnaval huipentuu Stadin kuumimpiin bileisiin, kun Baile do Carnaval valtaa Kulttuuritehdas Korjaamon lauantaina 14. kesäkuuta! Tiedossa on unohtumaton karnevaalitunnelma – sambarytmejä, näyttäviä esityksiä, live-musiikkia ja loistava DJ.<br><br> Liput tikestistä";
@@ -61,7 +61,7 @@ function KesaKalenteri4() {
 
 }
 function KesaKalenteri5() {
-    if (paivatanaan >= new Date("2025-04-16")) {
+    if (paivatanaan >= new Date("2025-06-16")) {
         document.getElementById("otsikko").innerHTML = "Seurasaaren juhannusvalkeat 20.6.2025 klo 17:00 – 21.6.2025 klo 01:00";
         document.getElementById("kuva").src = "kuvat/VK5.jpg";
         document.getElementById("kappale").innerHTML = "Seurasaaressa juhlitaan keskikesää ja tehdään elävää kulttuuriperintöä näkyväksi! Vehreä kansanpuisto ja idyllinen museoalue luovat erinomaiset puitteet viettää juhannusta. Juhannusvalkeilla ihastuttavat värikkäät kansallispuvut, taitavat kansantanssijat ja pelimannit kokkojen roihutessa. (Metsäpalovaroituksen voimassaoloaikana kokkoja ei voida sytyttää).<br><br>Paikka: Seurasaaren ulkomuseo";
@@ -73,7 +73,7 @@ function KesaKalenteri5() {
 
 }
 function KesaKalenteri6() {
-    if (paivatanaan >= new Date("2025-04-23")) {
+    if (paivatanaan >= new Date("2025-06-23")) {
         document.getElementById("otsikko").innerHTML = "Tuska 27.-29.06.2025";
         document.getElementById("kuva").src = "kuvat/VK6.jpg";
         document.getElementById("kappale").innerHTML = "Suvilahdessa järjestettävä Tuska Festival on nykyään Pohjoismaiden suurin metallifestivaali. Tänä vuonna esiintyjinä mm. Lorna Shore, In Flames, Electric Callboy, Powerwolf, Apocalyptica, Nothin More, Orbit Culture, Mokoma";
@@ -85,7 +85,7 @@ function KesaKalenteri6() {
 
 }
 function KesaKalenteri7() {
-    if (paivatanaan >= new Date("2025-04-30")) {
+    if (paivatanaan >= new Date("2025-06-30")) {
         document.getElementById("otsikko").innerHTML = "Kipinä Festival 04.-05.07.2025";
         document.getElementById("kuva").src = "kuvat/VK7.jpg";
         document.getElementById("kappale").innerHTML = "Kipinä Festivalit juhlitaan Helsingissä Oulunkylän urheilupuistossa.<br><br>Esiintyjinä nähdään perjantaina JVG, Earth, Wind & Fire Experience, Abreu, Isac Elliot, Elastinen, Viivi, Ege Zulu, Arttu Wiskari ja Jannika B. Lauantaina vuoronsa saavat Jenni Vartiainen, Gasellit, Mirella, Robin Packalen, Dire Straits Legacy, Kaija Koo, Ani, Karri Koira ja Tuuli.<br><br>Tietokirjailija Emilia Vuorisalmi tuo Kipinä Festivaaleille aivan uudenlaisen tilan, jossa festivaalivieraat saavat kokea uusia fiiliksiä ja löytää sisäisen kipinän. Tapahtumaan tulee myös käyttöön sinkkuasiakkaille suunnattu sinkkuranneke.";
@@ -97,7 +97,7 @@ function KesaKalenteri7() {
 
 }
 function KesaKalenteri8() {
-    if (paivatanaan >= new Date("2025-04-07")) {
+    if (paivatanaan >= new Date("2025-07-07")) {
         document.getElementById("otsikko").innerHTML = "Lonna";
         document.getElementById("kuva").src = "kuvat/VK8.jpg";
         document.getElementById("kappale").innerHTML = "Lonna on pieni, mutta monipuolinen saari Suomenlinnan lähellä. Sinne pääsee näppärästi vesibussilla Kauppatorilta. Lonnassa voi saunoa, hengailla ja nauttia saaren keittiön upeista tarjottavista. Saaressa järjestetään ulkoilmaelokuvailtoja, joogataan, vietetään erilaisia tapahtumia sekä juhlitaan juhannusta.";
@@ -109,7 +109,7 @@ function KesaKalenteri8() {
 
 }
 function KesaKalenteri9() {
-    if (paivatanaan >= new Date("2025-04-14")) {
+    if (paivatanaan >= new Date("2025-07-14")) {
         document.getElementById("otsikko").innerHTML = "Aleksis Kiven kadun kirppis 20.07.2025 ";
         document.getElementById("kuva").src = "kuvat/VK9.jpg";
         document.getElementById("kappale").innerHTML = "Dallapénpuistossa toukokuusta syyskuuhun sunnuntaisin järjestettävä ulkokirppis. Kuka tahansa voi mennä tavaroineen Aleksis kiven kadulle kirppispäivän ajaksi myymään, kunhan vie myymättömät tavarat mukanaan pois ja siivoaa jälkensä. Muistathan siis viedä paitsi roskasi mukanasi, myös myymättömät vaatteet ja muut tavarat.<br><br>Tapahtumasta on muodostunut myös ruokamatka lukuisten kulttuurien makuihin ja keittiöihin. Se onkin kaupungin vilkkain ja eläväisin katuruokatapahtuma kesäisin. Myös satokauden herkkuja, kuten loppukesästä kanttarelleja myydään kojuissa vain puolella Kauppatorin hinnoista.";
@@ -121,7 +121,7 @@ function KesaKalenteri9() {
 
 }
 function KesaKalenteri10() {
-    if (paivatanaan >= new Date("2025-04-21")) {
+    if (paivatanaan >= new Date("2025-07-21")) {
         document.getElementById("otsikko").innerHTML = "Smash into Pieces 24.07.2025 Allas Live K18";
         document.getElementById("kuva").src = "kuvat/VK10.jpg";
         document.getElementById("kappale").innerHTML = "Allas Live tarjoaa kävijöilleen kesän kuumimman keikkasarjan ja unohtumattomia hetkiä musiikin parissa! Ruotsalainen metalliyhtye Smash into Pieces esiintyy Allas Poolilla.";
@@ -133,7 +133,7 @@ function KesaKalenteri10() {
 
 }
 function KesaKalenteri11() {
-    if (paivatanaan >= new Date("2025-04-28")) {
+    if (paivatanaan >= new Date("2025-07-28")) {
         document.getElementById("otsikko").innerHTML = " Weekend Festival 1.-2.8.";
         document.getElementById("kuva").src = "kuvat/VK11.jpg";
         document.getElementById("kappale").innerHTML = "Järjestetään 1.-2.8 Vermon Tapahtumapuistossa Espoossa. Festivaalin ikäraja on 18+. Esiintyjinä mm. Armin van Buuren, Fisher, Hardwell ja Robin Shculz";
@@ -145,7 +145,7 @@ function KesaKalenteri11() {
 
 }
 function KesaKalenteri12() {
-    if (paivatanaan >= new Date("2025-04-04")) {
+    if (paivatanaan >= new Date("2025-08-04")) {
         document.getElementById("otsikko").innerHTML = "Kasarmitorin kesäterassi 12.06.-15.08.2025";
         document.getElementById("kuva").src = "kuvat/vko12.1.jpg";
         document.getElementById("kappale").innerHTML = "Torille on tulossa jopa 1000 asiakaspaikkaa ja 16 ravintolaa. Terassilla on ruoan ja juoman lisäksi luvassa ohjelmaa lähes joka päivä. Torille suunnitellaan ohjelmaa kaikenikäisille- Meet&greet -tapahtumia lapsille ja nuorille, sekä mummodiskoa ja yhteislaulutilaisuuksia varttuneemmille.<br><br>Ravintoloita mukana ovat mm. hienostunut Finnjävel, viinibaari Bricco, mehukkaita höyrytettyjä ja täytettyjä sämpylöitä loihtiva baobao, Kauppatorin mehevistä lihapiirakoista ja muista leivonnaisistaan tunnettu Eromanga, sekä Hampurilaisistaan palkittu ja Herttoniemestä ponnistava kortteliravintola Treffipub.";
@@ -156,7 +156,7 @@ function KesaKalenteri12() {
     }
 }
 function KesaKalenteri13() {
-    if (paivatanaan >= new Date("2025-04-11")) {
+    if (paivatanaan >= new Date("2025-08-11")) {
         document.getElementById("otsikko").innerHTML = "Elämyksellinen risteily Helsingin päänähtävyyksien varrella";
         document.getElementById("kuva").src = "kuvat/VK13.jpg";
         document.getElementById("kappale").innerHTML = "Risteily lähtee Kauppatorilta, ja kierros kulkee muun muassa kauniin Merisataman sekä Eiran, Kaivopuiston ja Kruununhaan rannat. Lisäksi risteily kiertää Suomenlinnan, UNESCO:n maailmanperintökohteen ympäri. <br><br>2.6.-28.9.2025 ma-su klo 11, 12, 13, 14, 15, 16 & 17 <br>Juhannusaattona 20.6.2025 poikkeuksia lähtöajoissa. <br><br>Kesto: 1,5 tuntia <br>Lähtöpaikka: Helsingin Kauppatori, Kolera-allas ";
